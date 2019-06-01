a4665fdb6dffa2c16ec524ef7c196b5c6d4e8f4f
GustavoMF31 <gustavodemendoncafreire@gmail.com>
2019-05-31 22:37:49 -0300

    Ask for input again if it's empty

--- human_player.rb
+++ human_player.rb
@@ -1,14 +1,16 @@
 class Human
     def get_move(board, player_num)
         puts board.to_s
-        puts "\n"
-        print "What column to play in?: "
-        col_index = gets.to_i
-        while col_index == ""
+        print "\nWhat column to play in?: "
+
+        col_index = gets
+
+        while col_index == "\n"
             print "What column to play in?: "
-            col_index = gets.to_i
+            col_index = gets
         end
+        
         puts "\n"
-        col_index
+        col_index.to_i
     end
 end
